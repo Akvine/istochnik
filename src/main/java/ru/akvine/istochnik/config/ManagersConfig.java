fa541611a3440b5846b338dc9fa15589da30baf6
akvine <redgooseman55@gmail.com>
2025-04-11 15:00:58 +0300

    replace manager config package

--- src/main/java/ru/akvine/istochnik/config/ManagersConfig.java
+++ src/main/java/ru/akvine/istochnik/config/ManagersConfig.java
@@ -1,4 +1,4 @@
-package ru.akvine.istochnik.services.config;
+package ru.akvine.istochnik.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
